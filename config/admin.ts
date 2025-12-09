8ba4796891cd5e37e255eea6b7219b9ee9f40993
Ojitha Rajapaksha <ojitha.20230522@iit.ac.lk>
2025-12-09 10:56:53 +0530

    feat(config): update admin JWT secret to fallback on JWT_SECRET

--- config/admin.ts
+++ config/admin.ts
@@ -1,6 +1,6 @@
 export default ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET') || env('JWT_SECRET'),
   },
   apiToken: {
     salt: env('API_TOKEN_SALT'),
